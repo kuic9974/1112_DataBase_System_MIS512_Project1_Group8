0f97b56f4738fb36e03beaa4f2dfae7fd4d6a8c6
MSI-WOODYCHEN\kuic9 <popper9200@gmail.com>
2023-04-30 11:49:47 +0800

    fix

--- link.py
+++ link.py
@@ -1,20 +1,20 @@
 import cx_Oracle
-import oracledb
+# import oracledb
 
 # 使用 oracledb 就不用Oracle Instant Client了
 # cx_Oracle.init_oracle_client(lib_dir="C:\Oracle\Client\instantclient-basic-windows.x64-19.18.0.0.0dbru\instantclient_19_18") # init Oracle instant client 位置
-# connection = cx_Oracle.connect('GROUP8', 'OfVkxNNOD5', cx_Oracle.makedsn('140.117.69.60', 1521, service_name='ORCLPDB1'))
+connection = cx_Oracle.connect('GROUP8', 'OfVkxNNOD5', cx_Oracle.makedsn('140.117.69.60', 1521, service_name='ORCLPDB1'))
 
 # 參數
-user = 'GROUP8'
-password = 'OfVkxNNOD5'
-dsn = cx_Oracle.makedsn('140.117.69.60', 1521, service_name='ORCLPDB1')
+# user = 'GROUP8'
+# password = 'OfVkxNNOD5'
+# dsn = cx_Oracle.makedsn('140.117.69.60', 1521, service_name='ORCLPDB1')
 
 # pool
-pool = oracledb.SessionPool(user=user, password=password, dsn=dsn, min=2, max=20, increment=2, encoding='UTF-8', nencoding='UTF-8', threaded=True, timeout=60)
+# pool = oracledb.SessionPool(user=user, password=password, dsn=dsn, min=2, max=20, increment=2, encoding='UTF-8', nencoding='UTF-8', threaded=True, timeout=60)
 
 # 連線 Oracle
-# connection = oracledb.connect(user=user, password=password, dsn=dsn)
-connection = pool.acquire()
+# connection = cx_Oracle.connect(user=user, password=password, dsn=dsn)conda create -n <name> python anaconda
+# connection = pool.acquire()
 
 cursor = connection.cursor()
