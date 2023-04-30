dedf69f31bc4a9f888013374d87153b876419196
MSI-WOODYCHEN\kuic9 <popper9200@gmail.com>
2023-04-30 14:28:33 +0800

    fix

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# 下載Oracle Instant Client
-wget https://download.oracle.com/otn_software/linux/instantclient/2110000/instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
-# 下載解壓縮套件
-# apt-get update
-# apt-get install unzip
-yum install unzip
-# 解壓縮檔案
-unzip instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
-# 設定環境變數
-# export LD_LIBRARY_PATH=/workspace/instantclient_21_9:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/workspace/instantclient_21_10:$LD_LIBRARY_PATH
+# # 下載Oracle Instant Client
+# wget https://download.oracle.com/otn_software/linux/instantclient/2110000/instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
+# # 下載解壓縮套件
+# # apt-get update
+# # apt-get install unzip
+# yum install unzip
+# # 解壓縮檔案
+# unzip instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
+# # 設定環境變數
+# # export LD_LIBRARY_PATH=/workspace/instantclient_21_9:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/workspace/instantclient_21_10:$LD_LIBRARY_PATH
