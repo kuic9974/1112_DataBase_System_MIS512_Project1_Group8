3932084745ca72f9aa7a1507eaaccf76bf4e8491
MSI-WOODYCHEN\kuic9 <popper9200@gmail.com>
2023-04-30 12:19:15 +0800

    fix

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 # 下載Oracle Instant Client
 wget https://download.oracle.com/otn_software/linux/instantclient/2110000/instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
 # 下載解壓縮套件
-apt-get update
+# apt-get update
 apt-get install unzip
 # 解壓縮檔案
 unzip instantclient-basic-linux.x64-21.10.0.0.0dbru.zip
